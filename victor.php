3834d916f2a7b0cc5566d0d4d68ddd15393add09
aluno <aluno@lab-wbs.local>
2016-03-30 08:26:56 -0300

    algoritmo par

---
+++ victor.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<title>primeira parte</title>
+	</head>
+	<body>
+	
+	<?php
+	$num1;
+	$num2 = 2;
+	
+	for($num1 = 1; $num1 < 20;$num1++ ){
+		if($num1 % $num2 == 0){
+			echo"Numero Par: ".$num1;
+		}
+		
+	}
+
+	?>
+	</body>
+</html>
+	
