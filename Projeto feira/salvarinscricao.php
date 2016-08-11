25760d9cdbc1d91cc006e742d62c9537d97c92c5
aluno <aluno@labwbs15>
2016-08-11 07:47:45 -0400

    atualizacao

--- Projeto feira/salvarinscricao.php
+++ Projeto feira/salvarinscricao.php
@@ -2,4 +2,16 @@
 	echo "GET:" . var_dump($_GET) . "<br>";
 	echo "POST:" . var_dump($_POST) . "<br>";
 	echo "posicao nome" . $_POST['nome'];
+	
+	/*configuraçao de endereçamento do banco de dados*/
+	$servidor="localhost";
+	$usuario ="root";
+	$senha ="";
+	
+	/*configuraçao do banco de dados*/
+	$nome_banco= "bd_centro_interesse";
+	
+	$conexao = mysql_conect($servidor,$usuario,$senha);
+	
+	$banco = mysql_select_bd($_banco,$conexao);
 ?>
